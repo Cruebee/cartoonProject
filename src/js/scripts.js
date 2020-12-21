e34c91d580270b11de81a52afa7f77379d3ccd06
Crue Jameson <ericcjameson@gmail.com>
2020-12-21 15:38:40 -0700

    add basic js object

---
+++ src/js/scripts.js
@@ -0,0 +1,62 @@
+let characters = [
+  {
+    id: 361,
+    name: 'Toxic Rick',
+    status: 'Dead',
+    species: 'Humanoid',
+    type: "Rick's Toxic Side",
+    gender: 'Male',
+    origin: {
+      name: 'Alien Spa',
+      url: 'https://rickandmortyapi.com/api/location/64',
+    },
+    location: {
+      name: 'Earth',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    image: 'https://rickandmortyapi.com/api/character/avatar/361.jpeg',
+    episode: ['https://rickandmortyapi.com/api/episode/27'],
+    url: 'https://rickandmortyapi.com/api/character/361',
+    created: '2018-01-10T18:20:41.703Z',
+  },
+  {
+    id: 362,
+    name: 'Toxic Morty',
+    status: 'Dead',
+    species: 'Humanoid',
+    type: "Morty's Toxic Side",
+    gender: 'Male',
+    origin: {
+      name: 'Alien Spa',
+      url: 'https://rickandmortyapi.com/api/location/64',
+    },
+    location: {
+      name: 'Earth',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    image: '',
+    episode: ['https://rickandmortyapi.com/api/episode/27'],
+    url: 'https://rickandmortyapi.com/api/characters/362',
+    created: '2018-01-10T18:20:41.703Z',
+  },
+  {
+    id: 363,
+    name: 'Evil Beth Monster',
+    status: 'Dead',
+    species: 'Modified Humanoid',
+    type: "Beth's Therapy Monster",
+    gender: 'Female',
+    origin: {
+      name: 'Alien Spa',
+      url: 'https://rickandmortyapi.com/api/location/64',
+    },
+    location: {
+      name: 'Space',
+      url: 'https://rickandmortyapi.com/api/location/22',
+    },
+    image: '',
+    episode: ['https://rickandmortyapi.com'],
+    url: 'https://rickandmortyapi.com/api/characters/363',
+    created: '2018-01-10T18:20:41.703Z',
+  },
+];
